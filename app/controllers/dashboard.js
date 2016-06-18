7fe8abfa0999f9285d25e922290c3399a6907763
piotrzientara <piotr.zientara@rebased.pl>
2016-06-18 12:56:05 +0200

    adds hash generator with unit test

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -3,11 +3,15 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
   user: Ember.computed.alias('session.currentUser'),
+  hash: Ember.computed('textInput', function() {
+    let hashids = new Hashids(this.get('textInput'), 8),
+        id = hashids.encode(1234);
+    return id;
+  }),
 
   actions: {
     getInput() {
-      console.log("gets input");
-      console.log("generates hash");
+      console.log("generates hash", this.get('hash'));
       console.log("stores hash");
       console.log("cleans fields");
     },
--- tests/unit/controllers/dashboard-test.js
+++ tests/unit/controllers/dashboard-test.js
@@ -1,12 +1,53 @@
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('controller:dashboard', 'Unit | Controller | dashboard', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
+var no_input_hash = 'KQe1ljdJ',
+    lorem = "Lorem ipsum Nostrud proident exercitation do Excepteur pariatur mollit magna Duis reprehenderit commodo sed culpa dolor ut.";
+
+test('computed hash correcty creates hash from empty input', function(assert) {
   let controller = this.subject();
+
+  assert.equal(controller.get('hash'), no_input_hash);
   assert.ok(controller);
+});
+
+test('computed hash correcty creates hash from some input', function(assert) {
+  let controller = this.subject();
+
+  controller.set('textInput', lorem);
+  assert.notEqual(controller.get('hash'), no_input_hash);
+  assert.ok(controller);
+});
+
+test('computed hash has 8 characters', function(assert) {
+  let controller = this.subject();
+
+  controller.set('textInput', lorem + lorem);
+  assert.equal(controller.get('hash').length, 8);
+});
+
+test('computed hash doesn\'t create less than 8 characters', function(assert) {
+  let controller = this.subject();
+
+  controller.set('textInput', '1');
+  assert.equal(controller.get('hash').length, 8);
+});
+
+test('creates the same hash from the same input', function(assert) {
+  let controller = this.subject();
+
+  controller.set('textInput', lorem);
+  let firstHash = controller.get('hash');
+
+  controller.set('textInput', 'different input');
+  let differentInput = controller.get('hash');
+
+  assert.notEqual(firstHash, differentInput);
+
+  controller.set('textInput', lorem);
+  let secondHash = controller.get('hash');
+
+  assert.equal(firstHash, secondHash);
 });
